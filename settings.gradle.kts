76047038296b6e20a90df076457d514d6bbd3b0a
Polaralias <76123410+Polaralias@users.noreply.github.com>
2025-10-25 03:49:39 +0100

    Refactor settings.gradle.kts for readability

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,16 @@
-rootProject.name = "YTFocus"
-include(":app")
 pluginManagement {
-    repositories { google(); mavenCentral(); gradlePluginPortal() }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
+rootProject.name = "YT Focus"
+include(":app")
