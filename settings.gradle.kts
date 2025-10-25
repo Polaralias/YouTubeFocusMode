dc44f732511903f7010bf06d003ec1b5ab427dd2
Polaralias <76123410+Polaralias@users.noreply.github.com>
2025-10-25 03:43:20 +0100

    Add plugin and dependency resolution management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
 rootProject.name = "YTFocus"
 include(":app")
+pluginManagement {
+    repositories { google(); mavenCentral(); gradlePluginPortal() }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories { google(); mavenCentral() }
+}
